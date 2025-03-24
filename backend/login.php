7f4d1b974d72ef579debbdb5d81accf5001d5c4b
illinai <144272615+illinai@users.noreply.github.com>
2025-03-23 19:22:23 -0700

    direct admin

--- backend/login.php
+++ backend/login.php
@@ -18,8 +18,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (password_verify($password, $user['password'])) {
             // Successful login
             $_SESSION['user_id'] = $user['id'];
-            header("Location: ../main/dashboard.html"); // Redirect to dashboard
-            exit();
+            $stmt2 = $conn->prepare("SELECT role FROME user WHERE id = ?");
+            $stmt2->bind_param("s", $user["id"]);
+            $stmt2->execute();
+            $result2 = $stmt2->get_result();
+            $role = $result2->fetch_assoc();
+            if ($role == "admin"){
+                header("Location: ../admin/adminDash.html"); // Redirect to admin dashboard
+                exit();
+            } else {
+                header("Location: ../main/dashboard.html"); // Redirect to dashboard
+                exit();
+            }
         } else {
             // Incorrect password
             header("Location: ../main/index.html?error=Invalid email or password");

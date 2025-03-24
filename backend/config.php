4aa306ac740c9793d39cbb12c36ff1a22d33f4e4
simerdeepmann <simerdeepmann01@gmail.com>
2025-03-23 18:53:34 -0700

    change config back to original

--- backend/config.php
+++ backend/config.php
@@ -1,4 +1,5 @@
 <?php
+// filepath: 
 $servername = "cosc360.ok.ubc.ca";
 $username = "vpanagsa";
 $password = "vpanagsa";
@@ -7,11 +8,10 @@ $dbname = "vpanagsa";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 
-// Debugging: Confirm connection
-echo "Connected successfully!";
+// Debugging information
+echo "<br>Server: $servername";
+echo "<br>Username: $username";
+echo "<br>Password: $password";
+echo "<br>Database: $dbname";
 ?>
